4960b4046680d92922ac111c8c3a51209f8b2e50
mofhasa <mofhasa@yahoo.com>
2020-03-14 13:17:28 -0400

    Init commit

---
+++ build/precache-manifest.bed25a2574577d5415f185199b7b19f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39b3871def12a779fd797c384c4820ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c135b2a8113eef23474",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "afcf1e83bc24ffabab2d",
+    "url": "/static/js/2.0c5a801e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.0c5a801e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c135b2a8113eef23474",
+    "url": "/static/js/main.00f62a68.chunk.js"
+  },
+  {
+    "revision": "d43f914342b662c76ec3",
+    "url": "/static/js/runtime-main.8c8accba.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
